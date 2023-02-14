7fd90e574d248a3540d870c7e27d10ec7b033c71
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-02-14 00:08:28 -0500

    complete WhoToFollow section

--- public/tuiter/WhoToFollowList/who.js
+++ public/tuiter/WhoToFollowList/who.js
@@ -1,15 +1,15 @@
 export default [
-  { avatarIcon: "java.png", userName: "Java", handle: "Java" },
+  { avatarIcon: "../assets/java.jpg", userName: "Java", handle: "Java" },
   {
-    avatarIcon: "relativity.jpeg",
+    avatarIcon: "../assets/relativityspace.jpg",
     userName: "Relativity Space",
     handle: "relativityspace",
   },
   {
-    avatarIcon: "virgin.png",
+    avatarIcon: "../assets/virgin.png",
     userName: "Virgin Galactic",
     handle: "virgingalactic",
   },
-  { avatarIcon: "nasa.png", userName: "NASA", handle: "NASA" },
-  { avatarIcon: "tesla.png", userName: "Tesla", handle: "Tesla" },
+  { avatarIcon: "../assets/nasa.png", userName: "NASA", handle: "NASA" },
+  { avatarIcon: "../assets/tesla.jpg", userName: "Tesla", handle: "Tesla" },
 ];
