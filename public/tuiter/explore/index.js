d7e7dbaae917a78a90364920d0c2e93700b7173d
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-02-14 13:18:42 -0500

    complete a5

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,4 +1,5 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
+import ExploreComponent from "./ExploreComponent.js"
 import WhoToFollowList from "../WhoToFollowList/index.js";
 
 function exploreComponent() {
@@ -7,10 +8,10 @@ function exploreComponent() {
    <div class="col-2 col-md-2 col-lg-1 col-xl-2">
     ${NavigationSidebar()}
    </div>
-   <div class="col-10 col-lg-7 col-xl-6 bg-primary ">
-    <h3>ExploreComponent</h3>
+   <div class="col-10 col-lg-7 col-xl-6">
+    ${ExploreComponent()}
    </div>
-   <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 text-white">
+   <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
     ${WhoToFollowList()}
    </div>
   </div>
