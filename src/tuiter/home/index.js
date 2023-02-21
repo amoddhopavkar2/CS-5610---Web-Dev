f4eb113efc9c8bd82a0c1e9c1b6df758bc23d060
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-02-21 13:36:30 -0500

    start a6 assignment

---
+++ src/tuiter/home/index.js
@@ -0,0 +1,15 @@
+import tweetsArray from "./tweets.json";
+import TweetSummaryItem from "./tweet-summary-item";
+import "./index.css";
+
+const HomeComponent = () => {
+  return (
+    <div className="wd-posts">
+      {tweetsArray.map((post) => (
+        <TweetSummaryItem key={post._id} tweet={post} />
+      ))}
+    </div>
+  );
+};
+
+export default HomeComponent;
--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,9 +1,30 @@
+import { Link, Route } from "react-router-dom";
 import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+import ExploreComponent from "./explore";
+import WhoToFollowList from "./who-to-follow-list";
+import { Routes } from "react-router-dom";
+import HomeComponent from "./home";
+
 function Tuiter() {
   return (
-    <div>
-      <Nav />
-      <h1>Tuiter</h1>
+    <div className="row mt-2">
+      <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+        <Nav />
+        <NavigationSidebar active="explore" />
+      </div>
+      <div
+        className="col-10 col-md-10 col-lg-7 col-xl-6"
+        style={{ position: "relative; padding:0px;border:1px solid black" }}
+      >
+        <Routes>
+          <Route path="explore" element={<ExploreComponent />} />
+          <Route path="home" element={<HomeComponent />} />
+        </Routes>
+      </div>
+      <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+        <WhoToFollowList />
+      </div>
     </div>
   );
 }
