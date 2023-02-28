bf19f4e3706d206f50012affeb1662f2af066fb3
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-02-28 16:54:19 -0500

    start a6 challenge

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
       <div className="container">
         <Routes>
           <Route index element={<Labs />} />
-          <Route path="/hello/" element={<HelloWorld />} />
-          <Route path="/tuiter/" element={<Tuiter />} />
+          <Route path="/hello" element={<HelloWorld />} />
+          <Route path="/tuiter/*" element={<Tuiter />} />
         </Routes>
       </div>
     </BrowserRouter>
