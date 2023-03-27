ef85b0307fafe99f9814b73febe6209e3e4278ff
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-03-27 17:35:09 -0400

    almost there

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import TuitList from "../tuits/tuit-list";
 import WhatsHappening from "./whats-happening";
 const HomeComponent = () => {
-    return (
-        <>
-            <h4>Home</h4>
-            <WhatsHappening />
-            <TuitList />
-        </>);
+  return (
+    <>
+      <h4>Home</h4>
+      <WhatsHappening />
+      <TuitList />
+    </>
+  );
 };
 export default HomeComponent;
--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -2,25 +2,20 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { findTuitsThunk } from "../../services/tuits-thunks";
 
-import PostSummaryItem
-  from "./post-summary-item";
+import PostSummaryItem from "./post-summary-item";
 
 const PostSummaryList = () => {
   const { tuits, loading } = useSelector((state) => state.tuitsData);
   const dispatch = useDispatch();
   useEffect(() => {
-      dispatch(findTuitsThunk())
-  }, [])
-  // const postsArray = useSelector(state => state.tuitsData)
+    dispatch(findTuitsThunk());
+  }, []);
   return (
     <ul className="list-group">
-      {
-        tuits.map(post =>
-          <PostSummaryItem
-            key={post._id} post={post} />)
-      }
+      {tuits.map((post) => (
+        <PostSummaryItem key={post._id} post={post} />
+      ))}
     </ul>
   );
 };
 export default PostSummaryList;
-
