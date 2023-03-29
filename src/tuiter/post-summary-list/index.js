52e7ea6521dd5f5abdd8bc519fc16e2a8872eb09
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-03-29 00:08:58 -0400

    almost there

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -12,6 +12,7 @@ const PostSummaryList = () => {
   }, []);
   return (
     <ul className="list-group">
+      console.log(tuits.map)
       {tuits.map((post) => (
         <PostSummaryItem key={post._id} post={post} />
       ))}
