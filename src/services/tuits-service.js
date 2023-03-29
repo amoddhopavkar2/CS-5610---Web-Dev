4fc0e20b769f01f31ec2f3a2d71fea75c3c52962
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-03-29 00:03:53 -0400

    almost there

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,25 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 const API_BASE = process.env.MONGO_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
-    const response = await axios.post(TUITS_API, tuit)
-    return response.data;
-}
+  const response = await axios.post(TUITS_API, tuit);
+  return response.data;
+};
 
 export const findTuits = async () => {
-    const response = await axios.get(TUITS_API);
-    const tuits = response.data;
-    return tuits;
-}
+  const response = await axios.get(TUITS_API);
+  const tuits = response.data;
+  return tuits;
+};
 
 export const deleteTuit = async (tid) => {
-    const response = await axios
-        .delete(`${TUITS_API}/${tid}`)
-    return response.data
-}
+  const response = await axios.delete(`${TUITS_API}/${tid}`);
+  return response.data;
+};
 
 export const updateTuit = async (tuit) => {
-    await axios.put(`${TUITS_API}/${tuit._id}`, tuit);
-    return tuit;
-}
+  await axios.put(`${TUITS_API}/${tuit._id}`, tuit);
+  return tuit;
+};
